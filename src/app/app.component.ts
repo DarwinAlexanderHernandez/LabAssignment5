54b91b2a4d2d4edc89f071fdfda14b9ecd92a261
Darwin Hernandez <Dh0740226@mail.fresnostate.edu>
2019-04-16 13:06:29 -0700

    add main header, footer and content component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'LabAssingment3';
+  title = 'LabAssignment3';
+
+  constructor() {
+
+  }
+
+
 }
